1153c6c11371c1fd803dd79691762207d874f8cd
kierstendecamp <decampkk@g.cofc.edu>
2022-01-21 21:28:49 -0500

    hw 1

---
+++ notes/class.py
@@ -0,0 +1,10 @@
+def convert():
+    # user input in celcius
+    celcius = eval(input("what is the temperature in celcius ?"))
+    # convert fahrenheit to celcius
+    fahrenheit = celcius * 9/5 + 32
+    # display result
+
+
+    print("the temperature is", fahrenheit, "degrees fahrenheit. Awesome!")
+ 
