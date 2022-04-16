f7b244e720523e4093e47303365d9dbc6883b4d0
kierstendecamp <decampkk@g.cofc.edu>
2022-04-15 21:33:31 -0400

    commit salesperson

--- assignments/hw11/sales_force.py
+++ assignments/hw11/sales_force.py
@@ -1,9 +1,37 @@
-def count(file_name):
- # open the file
-# read all the lines
-# make all lowercase
-# replace all punctuation
-# split into lists of words
-# count all words - use a dictionary
-# sort word counts
-# display/ print
+class SalesForce:
+
+def __init__(self, sales_people):
+    people = self.sales_people = []
+
+def add_data(self, file_name):
+    open_file = (file_name, 'r')
+    read = open_file.readlines()
+    people = self.people.append(open_file)
+
+def quota_report(self, quota):
+    list_1 = []
+    for i in self.sales_people:
+        list_2 = []
+        list_1.append(i.get_id())
+        list_1.append(i.total_sales())
+        list_1.append(i.quota())
+        list_2.append(list_1())
+    return list_2
+def top_seller(self):
+    i = 0
+    for i in self.sales_people():
+        return i
+def individual_sales(self, employee_id):
+    for i in self.sales_people():
+        return i
+
+def get_sale_frequencies(self, sales_people, other):
+    for i in self.sales_people:
+        sales_1 = i.get_sales
+        value = 1
+        my_list = []
+        for other in self.sales_people():
+            if sales_1 == i.get_sales():
+                my_list.append(value)
+                sales_1.append(value)
+        return my_list
--- assignments/hw11/sales_person.py
+++ assignments/hw11/sales_person.py
@@ -1,2 +1,57 @@
+"""
+Name: kiersten decamp
+hw11.py
+
+Problem: <create a program using objects with lists.>
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
 class SalesPerson:
-    pass
+    def __init__(self, employee_id, name):
+        self.employee_id = employee_id
+        self.name = name
+        self.sales = []
+
+    def get_id(self, employee_id):
+        self.name = employee_id
+
+    def get_name(self, name):
+       return self.name
+
+
+    def set_name(self, name):
+        self.name = input("Kiersten DeCamp")
+        return name
+
+
+    def enter_sale(self, sale):
+        self.sales.append(sale)
+
+
+    def total_sale(self, sale):
+        my_sale = 0
+        return my_sale(self.sale)
+
+    def get_sales(self,sales):
+        i = []
+        for i in range(len(float(self.sales))):
+            if self.sales[i] >= sales:
+                return i
+
+
+    def met_quota(self):
+        return self.quota_sales()
+
+
+    def compare_to(self, other):
+        i = -1
+        for i in len(self.other):
+            if self.sales[i] >= other:
+                return i
+
+
+
+
